bb402283484de30ace65b2232fe4e20693c3eb37
Dady <ddady1@gmail.com>
2022-06-25 20:31:46 +0300

    7.3.2-R.py

---
+++ 7.3.2-R.py
@@ -0,0 +1,17 @@
+def check_win(secret_word, old_letters_guessed):
+
+    answer = True
+    for n in secret_word:
+
+        if n in old_letters_guessed:
+            answer = True
+        else:
+            answer = False
+            return answer
+    return answer
+
+
+
+secret_word = "plant"
+old_letters_guessed = ['m', 'p', 'j', 'i', 's', 'k', 'n', 'f', 'e', 'r', 'd', 'y']
+print(check_win(secret_word, old_letters_guessed))
