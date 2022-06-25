0cf8462e3286d2fbe910db7860e20899cbbe079a
Dady <ddady1@gmail.com>
2022-06-25 16:47:12 +0300

    7.2.7 Finished

--- 7.2.7.py
+++ 7.2.7.py
@@ -1,12 +1,17 @@
 def arrow(my_char, max_length):
 
-    #new_str = '1'
-    for n in range(max_length):
+    new_str = ''
+    for n in range(1, max_length + 1):
         new_str += (my_char * n)
+        if n == max_length:
+            break
         new_str += ('\n')
         n += 1
+
     while max_length > 0:
-        new_str += (my_char * max_length + '\n')
+        if max_length == 1:
+            break
+        new_str += ('\n' + my_char * (max_length -1))
         max_length -= 1
     return new_str
 
