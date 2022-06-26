4e2afefa493ae0ad0824297eceefac285ce52267
Dady <ddady1@gmail.com>
2022-06-26 22:59:09 +0300

    8.2.2 cont.

--- 8.2.2.py
+++ 8.2.2.py
@@ -1,9 +1,22 @@
 def sort_prices(list_of_tuples):
-    sorted_lst = list_of_tuples.sort(reverse=True, key=[1])
-    '''for n in list_of_tuples:
-        n[1] = float(n[1])
-        print(type(n[1]))'''
-    print(sorted_lst)
+    sorted_lst = []
+    flag = len(list_of_tuples)
+    #sorted_lst = list_of_tuples.sort()
+    for n in list_of_tuples:
+        num = float(n[1])
+        if n[0] == 0:
+
+            sorted_lst.append(n)
+        else:
+            if num > sorted_lst[1]:
+                sorted_lst.append(n)
+            #else:
+
+
+
+        print(type(num))
+        print(num)
+    #print(sorted_lst)
 
 
 
