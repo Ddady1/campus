abbb1fc29119edf6b1c4e161a8d94ee8abd66c7e
Dady <ddady1@gmail.com>
2022-07-01 18:25:56 +0300

    8.3.2

---
+++ 8.3.2.py
@@ -0,0 +1,15 @@
+famous = {'first_name': 'Mariah', 'last_name': 'Carey', 'birth_date': '27.03.1970', 'hobbies': ['Sing', 'Compose', 'Act']}
+x = int(input('Please enter a number between 1 and 7: '))
+if x == 1:
+    print(famous['last_name'])
+if x == 2:
+    print(famous['birth_date'])
+if x == 3:
+    print(len(famous['hobbies']))
+if x == 4:
+    f = len(famous['hobbies'])
+    print(famous['hobbies'][f - 1])
+if x == 5:
+    famous['hobbies'].append('Cooking')
+if x == 6:
+
