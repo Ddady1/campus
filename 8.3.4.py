0ae3a9a02b142746e150304875c6f90462a9dbe7
Dady <ddady1@gmail.com>
2022-07-03 22:42:32 +0300

    8.3.4

--- 8.3.4.py
+++ 8.3.4.py
@@ -9,9 +9,15 @@ def inverse_dict(my_dict):
 
     temp = l_lst[0]
 
-    for n in range(1, len(l_lst)):
+    for n in range(1, len(l_lst) -1):
         if l_lst[n][0] == temp[0]:
             temp.append(l_lst[n][1])
+            l_lst.remove(l_lst[n])
+
+    for f in l_lst:
+        for t in range(len(f) - 1):
+            new_dict.update({f[0]: f[t + 1]}) #f[0]] = f[t + 1]
+
 
     print(l_lst)
     return new_dict
