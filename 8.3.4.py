08b7fb20140504b67c81fd353e7db0235a01189f
dady <ddady1@gmail.com>
2022-07-04 13:40:23 +0300

    1.4.2 - 8.3.4 - 8.4.1

--- 8.3.4.py
+++ 8.3.4.py
@@ -8,11 +8,18 @@ def inverse_dict(my_dict):
         l_lst.append(lst)
 
     temp = l_lst[0]
+    ch_lst = []
     print(l_lst)
-    for n in range(1, len(l_lst) -1):
+    '''for n in range(1, len(l_lst) -1):
         if l_lst[n][0] == temp[0]:
             temp.append(l_lst[n][1])
-            l_lst.remove(l_lst[n])
+            l_lst.remove(l_lst[n])'''
+
+    for n in l_lst:
+        if n[0] not in ch_lst:
+            ch_lst.append(n[1])
+
+
 
 
     for f in l_lst:
@@ -28,5 +35,5 @@ def inverse_dict(my_dict):
 
 
 
-course_dict = {'I': 3, 'love': 2, 'self.py!': 2}
+course_dict = {'I': 3, 'love': 3, 'self.py!': 2}
 print(inverse_dict(course_dict))
---
+++ 8.4.1.py
@@ -0,0 +1,43 @@
+def print_hangman(num_of_tries):
+    HANGMAN_PHOTOS = {6: '''    x-------x
+    |       |
+    |       0
+    |      /|\\
+    |      /
+    |''', 4: """    x-------x
+    |       |
+    |       0
+    |       |
+    |
+    |""", 1: '''    x-------x''', 2: """    x-------x
+    |
+    |
+    |
+    |
+    |""", 3: """    x-------x
+    |       |
+    |       0
+    |
+    |
+    |""", 5: """    x-------x
+    |       |
+    |       0
+    |      /|\\
+    |
+    |""", 7: """    x-------x
+    |       |
+    |       0
+    |      /|\\
+    |      / \\
+    |"""}
+
+
+
+    print(HANGMAN_PHOTOS.get(num_of_tries))
+
+
+
+
+
+num_of_tries = 4
+print_hangman(num_of_tries)
