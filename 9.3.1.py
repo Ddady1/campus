bc72e701a16521208a0bf0d7494566bb14514007
Dady <ddady1@gmail.com>
2022-07-06 22:27:11 +0300

    9.3.1 Done!

--- 9.3.1.py
+++ 9.3.1.py
@@ -16,13 +16,23 @@ def my_mp3_playlist(file_path):
         for song in songs:
             if float(song[len(song) - 1]) > a:
                 a = float(song[len(song) - 1])
+                song_len = song[0]
         b = len(songs)
 
-        c = 0
-        for n in songs:
-            for nf in n:
 
-    tup = (str(a), b, c)
+        band_str = []
+        for n in songs:
+            band_str.append(n[1])
+        c = 0
+        for fl in range(len(band_str)):
+            num = band_str.count(band_str[fl])
+            if num > c:
+                band_name = band_str[fl]
+                c = num
+            fl += 1
+
+
+    tup = (song_len, b, band_name)
     return tup
 
 
