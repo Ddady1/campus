0a38af7da909742e18f78fcda15996ad241fbaf3
dady <ddady1@gmail.com>
2022-07-07 15:01:41 +0300

    9.3.2 15:01

--- 9.3.2.py
+++ 9.3.2.py
@@ -3,27 +3,28 @@ def my_mp4_playlist(file_path, new_song):
 
     with open(file_path, 'r') as f_read:
         data = f_read.readlines()
+        print(data)
 
-    #with open(file_path, 'w') as f_write:
         num = len(data)
-        f_data = data[num - 1].split(';')
-        f_data.pop(0)
-        f_data.insert(0, new_song)
-        f_data = ';'.join(f_data)
-
         if num < 3:
-            data.append( (3 - num) * '\n')
+            data.append((3 - num) * '\n')
             data.append(new_song)
         else:
+            f_data = data[2].split(';')
+            f_data.pop(0)
+            f_data.insert(0, new_song)
+            f_data = ';'.join(f_data)
             data.append(f_data)
 
-    with open( file_path, 'w') as f_write:
-        f_write.write(data)
+''' Need to convert data to string in order to write to file
+or run loop through data and write each line to file'''
 
-        print(len(data))
+
+
+        print(num)
         print(data[2])
         print(len(data[2]))
-        print(f_data)
+        #print(f_data)
         print(data)
 
 
