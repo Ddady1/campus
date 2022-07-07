c25ea4c02f59fb4657bb6186804c20a9b09d99e7
dady <ddady1@gmail.com>
2022-07-07 11:19:37 +0300

    9.3.1 15:51

--- 9.3.2.py
+++ 9.3.2.py
@@ -5,12 +5,27 @@ def my_mp4_playlist(file_path, new_song):
         data = f_read.readlines()
 
     #with open(file_path, 'w') as f_write:
-        f_data = data[2].split(';')
+        num = len(data)
+        f_data = data[num - 1].split(';')
         f_data.pop(0)
         f_data.insert(0, new_song)
+        f_data = ';'.join(f_data)
+
+        if num < 3:
+            data.append( (3 - num) * '\n')
+            data.append(new_song)
+        else:
+            data.append(f_data)
+
+    with open( file_path, 'w') as f_write:
+        f_write.write(data)
+
+        print(len(data))
         print(data[2])
         print(len(data[2]))
         print(f_data)
+        print(data)
+
 
 
 
