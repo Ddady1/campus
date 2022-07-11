612dc872961d77f14db808d4041b173f23c80260
dady <ddady1@gmail.com>
2022-07-11 15:36:31 +0300

    Hangman-Final v.11 with updates of comments

--- Hangman-Final.py
+++ Hangman-Final.py
@@ -35,12 +35,28 @@ def hang_photos(photo_num):
 
 def choose_word(file_path, index):
 
+    '''function the gets a file and return a word from its content
+    :param file_path: file path
+    :param index: the index valuse of the word
+    :type file_path: str
+    :type index: enumerate
+    :return: the word chosen
+    :rtype: str'''
+
     with open(file_path, 'r') as word_read:
         content = word_read.read()
         items = content.split(' ')
         return items[int(index) - 1]
 
 def check_win(secret_word, old_letters_guessed):
+
+    '''Function check if user won or lost
+    :param secret_word: secret word value
+    :param old_letters_guessed: old letters guessed
+    :type secret_word: str
+    :type old_letters_guessed: list
+    :return: result of checking
+    :rtype: bool '''
 
     answer = True
     for n in secret_word:
@@ -55,6 +71,14 @@ def check_win(secret_word, old_letters_guessed):
 
 def show_hidden_word(secret_word, old_letters_guessed):
 
+    ''' Function returns secret word only with the found letters
+    :param secret_word: secret word value
+    :param old_letters_guessed: old letters guessed
+    :type secret_word: str
+    :type old_letters_guessed: list
+    :return: result string
+    :rtype: str'''
+
     new_lst = list(secret_word)
     l_str = ['_'] * len(secret_word)
     for n in old_letters_guessed:
@@ -67,13 +91,17 @@ def show_hidden_word(secret_word, old_letters_guessed):
     return l_str
 
 def try_update_letter_guessed(letter_guessed, old_letters_guessed, secret_word):
-    ''' Function check if input is valid and tries to update old_letters_guessed.
+
+    ''' Function check if input is valid and tries to update old_letters_guessed. It also prints status and send another function to check
+    if the game ends.
     :param letter_guessed: guessed letter value
     :param old_letters_guessed: old letters guessed
+    :param secret_word: secret word value
     :type letter_guessed: str
     :type old_letters_guessed: list
+    :type secret_word: str
     :return: result if input is valid
-    :rtype: booelean
+    :rtype: str
     '''
 
     letter_guessed = letter_guessed.lower()
@@ -82,13 +110,11 @@ def try_update_letter_guessed(letter_guessed, old_letters_guessed, secret_word):
         print('X')
         print(' -> '.join(old_letters_guessed))
 
-        #return ('X', ' -> '.join((old_letters_guessed)))
         return 'no_good'
     elif len(letter_guessed) >= 2 or not letter_guessed.isalpha():
         print('X')
         print(' -> '.join(old_letters_guessed))
         return 'no_good'
-        #return ('X', ' -> '.join((old_letters_guessed)))
 
     elif letter_guessed in secret_word:
         old_letters_guessed.append(letter_guessed)
@@ -121,13 +147,10 @@ def hangma_main_pic():
 def main():
     MAX_TRIES = 6
     print(hangma_main_pic(), MAX_TRIES)
-    #print(hang_photos(3))
     f_path = input('Please enter file path: ')
     word_num = input('Please enter a num: ')
     secret_word = choose_word(f_path, word_num)
-    #print(secret_word)
-    #print(r'\n\n Let's Start!\n')
-    print(hang_photos(1))
+    print("""\nLet's Start!\n\n""", hang_photos(1))
     print('_' * len(secret_word))
     old_letters_guessed = []
     num_of_tries = 0
@@ -149,18 +172,6 @@ def main():
             print(hang_photos(num_of_tries + 1))
     print('Lose')
     exit()
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
