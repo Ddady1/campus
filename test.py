2e3fdfac46160aea080f690ab2d0b46667f799d5
Dady <ddady1@gmail.com>
2022-07-13 23:34:59 +0300

    1.1.2

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-x = ['Tudo Bom', 'Static and Ben El Tavori', '5:13']#, 'I Gotta Feeling;The Black Eyed Peas;4:05;', 'Instrumental;Unknown;4:15;', 'Paradise;Coldplay;4:23;', 'Where is the love?;The Black Eyed Peas;4:13;']
-t = x.count('Static and Ben El Tavori')
-print(t)
+import functools
+def f(a, b):
+    if a < b:
+        return a
+    else:
+        return b
+
+
+l = [47,11,42,102,13]
+print(functools.reduce(f, l ,10))
