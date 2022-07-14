361ec4995329bd7dad7dbb0645283eb3dbb67972
Dady <ddady1@gmail.com>
2022-07-14 23:06:17 +0300

    1.1.2 Done!

--- 1.1.2.py
+++ 1.1.2.py
@@ -1,11 +1,15 @@
-import functools
+def duplicate (num):
+    #print(num)
+    return num * 2
+
 def double_letter(my_str):
-
-    return (my_str * 2)
-
-
+    #print(my_str)
+    return ''.join(list(map(duplicate, my_str)))
 
 
 
-print(list(map(double_letter, "python")))
-#print(list(map(double_letter, ("we are the champions!"))))
+
+
+print(double_letter("python"))
+
+print(double_letter("we are the champions!"))
