6e574d02ecdd0028c67aada65f3a3a23fb833fc0
dady <ddady1@gmail.com>
2022-07-14 14:16:47 +0300

    1.1.2

--- 1.1.2.py
+++ 1.1.2.py
@@ -1,11 +1,11 @@
 import functools
 def double_letter(my_str):
 
-    return my_str.join(my_str * 2)
+    return (my_str * 2)
 
 
 
 
 
-print(list(filter(double_letter, "python")))
+print(list(map(double_letter, "python")))
 #print(list(map(double_letter, ("we are the champions!"))))
