93d3662b426cb82685edc3b363f3d20645cb8d97
Dady <ddady1@gmail.com>
2022-07-15 17:10:23 +0300

    1.1.4 Done! + lambda example

--- 1.1.4.py
+++ 1.1.4.py
@@ -7,7 +7,7 @@ def sumall(mis1, mis2):
 def sum_of_digits(number):
 
     return functools.reduce(sumall, str(number))
-
+    # can be done with lambda instead of sumall finction. lambda x, y: int(x) + int(y)
 
 
 
