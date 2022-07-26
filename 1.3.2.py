9a0eede7562529cc99ee504318ff194fd718c6b8
Dady <ddady1@gmail.com>
2022-07-26 22:35:44 +0300

    1.3.2

--- 1.3.2.py
+++ 1.3.2.py
@@ -1,6 +1,6 @@
 def is_prime(number):
 
-    result = [False if x % 2 == 0 else True for x in range(2, number)]
+    result = [False if number % x == 0 else True for x in range(2, number)]
     return result
 
 
@@ -12,5 +12,5 @@ def is_prime(number):
 
 
 
-print(is_prime(42))
-print(is_prime(43))
+print(is_prime(25))
+print(is_prime(24))
