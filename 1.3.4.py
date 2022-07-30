6fade58996d576c44e7881b69e99e5e2e358a629
Dady <ddady1@gmail.com>
2022-07-30 23:37:21 +0300

    1.3.4 make better

--- 1.3.4.py
+++ 1.3.4.py
@@ -1,7 +1,8 @@
 password = "sljmai ugrf rfc ambc: lglc dmsp mlc rum"
 
 
-print([chr(ord(char) + 2) for char in password if char.isascii()])
+print(''.join([chr(ord(char) + 2) for char in password if char.isalpha()]))
+#print(''.join(([chr(ord(char) + 2) if char.isalpha else char.replace(':', '') for char in password])))
 
 ''' k - > m
     o - > q
