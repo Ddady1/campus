79eadd90266d7aea2c916ca36d0216c9e43ea6ae
dady <ddady1@gmail.com>
2022-08-01 15:12:00 +0300

    1.5B1

--- 1.5.py
+++ 1.5.py
@@ -1,5 +1,6 @@
-with open('names.txt', 'r') as f:
-    print(max(f.readlines()))
+'''with open('names.txt', 'r') as f:
+    print(max(f.readlines()))'''
 
 with open('names.txt', 'r') as f:
-    print(len(f.read()))
+    #print(sum(f.readlines()))
+    print(sum([len(i) - 1 for i in f.readlines()]))
